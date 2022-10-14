6874f81b69f56e3569cc07a7149a13f735e984c1
salinpaul94 <salinpv@hotmail.com>
2022-10-14 13:19:12 +0530

    class component

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 
 // import el from './10-react-fragment/react-fragment'
 
-import el from './12-components/components/slide-elements'
+// import el from './12-components/components/slide-elements'
+import el from './12-components/components/slide-elements-class'
 
 ReactDOM.render( el, document.getElementById( 'root' ) );
